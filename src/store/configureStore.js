0b6d2ed8a4e08388cdbb55a70946405d177b7267
Qurban Qurbanov <qurban.qurbanov93@gmail.com>
2016-12-15 14:58:06 +0400

    Reducer updated

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers'
 import createLogger from 'redux-logger'
+import thunk from 'redux-thunk'
 
 
 export default function configureStore(initialState) {
@@ -13,7 +14,7 @@ export default function configureStore(initialState) {
     if (module.hot) {
         module.hot.accept('../reducers', () => {
             const nextRootReducer = require('../reducers')
-            store.replaceReducer(nextRootReducer)
+            store.replaceReducer(thunk, nextRootReducer)
         })
     }
 
